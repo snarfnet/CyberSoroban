6ade4e26446230eff40ada8cf733916eea770e30
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-12 17:20:18 +0900

    fix: move MobileAds init to App.init, use onAppear for ATT

--- CyberSoroban/CyberSorobanApp.swift
+++ CyberSoroban/CyberSorobanApp.swift
@@ -4,23 +4,17 @@ import GoogleMobileAds
 
 @main
 struct CyberSorobanApp: App {
-    @Environment(\.scenePhase) private var scenePhase
-    @State private var attRequested = false
+    init() {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.dark)
-                .onChange(of: scenePhase) {
-                    if scenePhase == .active && !attRequested {
-                        attRequested = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                            ATTrackingManager.requestTrackingAuthorization { _ in
-                                DispatchQueue.main.async {
-                                    GADMobileAds.sharedInstance().start { _ in }
-                                }
-                            }
-                        }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        ATTrackingManager.requestTrackingAuthorization { _ in }
                     }
                 }
         }
